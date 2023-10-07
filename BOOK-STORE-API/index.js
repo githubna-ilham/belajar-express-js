82cb9d2f23244568883b480ef382f71413829b9c
gitlabna-ilham <surelna.ilham@gmail.com>
2023-10-07 17:14:41 +0700

    basic route get

--- BOOK-STORE-API/index.js
+++ BOOK-STORE-API/index.js
@@ -1,10 +1,19 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
-const port = 3333
+const app = express();
+const port = 3333;
 
-app.get('/',(req,res) =>{
-    res.send("Hello ini express lewat nodemon test")
-})
+app.get("/", (req, res) => {
+  res.send("Hello ini express lewat nodemon test");
+});
 
-app.listen(port,() => console.log(`Server running on port ${port}`))
+app.get("/users", (req, res) => {
+  res.send("Ini tampilan user");
+});
+
+app.get("/users/:id", (req, res) => {
+  let id = req.params;
+  res.send("Ini adalah data user nomor 1");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
