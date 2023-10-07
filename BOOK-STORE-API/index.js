8c527ea8441330fff8fdadd04a7e0b355205dc8e
githubna-ilham <ilham.anugrah@unpas.ac.id>
2023-10-07 11:26:18 +0700

    nodemon letsgo

--- BOOK-STORE-API/index.js
+++ BOOK-STORE-API/index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 3333
 
 app.get('/',(req,res) =>{
-    res.send("Hello ini express")
+    res.send("Hello ini express lewat nodemon test")
 })
 
 app.listen(port,() => console.log(`Server running on port ${port}`))
