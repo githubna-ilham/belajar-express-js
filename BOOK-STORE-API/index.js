8db8977c33add8d4c2629413ca086aa393e613a3
gitlabna-ilham <surelna.ilham@gmail.com>
2023-10-08 19:26:20 +0700

    error data found

--- BOOK-STORE-API/index.js
+++ BOOK-STORE-API/index.js
@@ -42,6 +42,8 @@ app.get("/users/:id", (req, res) => {
       status: 200,
       data: user,
     };
+  } else {
+    res.status(404).json({ error: "User not found" });
   }
   res.json(result);
 });
